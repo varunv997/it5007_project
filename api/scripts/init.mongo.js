31f09567705ad032621f57c351b954c543a521ac
Varun Narayanan <varunv997@outlook.com>
2022-04-22 17:29:27 +0800

    Add game db support

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -2,14 +2,14 @@ db.games.remove({});
 db.users.remove({});
 
 const gamesDB = [
-  { id: "1", name: "Chrome Dino", genre: "Arcade", posterPath: "images/chrome_dino.png", caption: "Take your dino for a ride in this browser based game!" },
-  { id: "2", name: "Splintercell Blacklist", genre: "Stealth", posterPath: "images/splintercell.jpg", caption: "You are blacklisted, but are you loyal to your country? (Coming soon)" },
-  { id: "3", name: "Arkham City", genre: "Action", posterPath: "images/arkhamcity.jpg", caption: "Joker has taken control of Arkham city, will Batman save the night? (Coming soon)" },
-  { id: "4", name: "The Witcher 3", genre: "Fantasy", posterPath: "images/witcher3.jpg", caption: "Save your protege, will you? (Coming soon)" },
-  { id: "5", name: "Age of Empires 4", genre: "Strategy", posterPath: "images/aoe4.jpg", caption: "Build your civilization, preserve it, conquer others in AOE IV (Coming soon)" },
-  { id: "6", name: "Fifa 22", genre: "Sports", posterPath: "images/fifa22.jpeg", caption: "Experience Football with FIFA 22 (Coming soon)" },
-  { id: "7", name: "NFSMW", genre: "Racing", posterPath: "images/nfsmw.jpg", caption: "Welcome to Rockport City, the speed has no limit here! (Coming soon)" },
-  { id: "8", name: "GTA 5", genre: "Open World Sandbox", posterPath: "images/gta5.jpg", caption: "Master the sandbox world in GTA V (Coming soon)" },
+  { id: "1", name: "Chrome Dino", genre: "Arcade", posterPath: "images/chrome_dino.png", caption: "Take your dino for a ride in this browser based game!", path: "/services/dino" },
+  { id: "2", name: "Splintercell Blacklist", genre: "Stealth", posterPath: "images/splintercell.jpg", caption: "You are blacklisted, but are you loyal to your country? (Coming soon)", path: "#" },
+  { id: "3", name: "Arkham City", genre: "Action", posterPath: "images/arkhamcity.jpg", caption: "Joker has taken control of Arkham city, will Batman save the night? (Coming soon)", path: "#" },
+  { id: "4", name: "The Witcher 3", genre: "Fantasy", posterPath: "images/witcher3.jpg", caption: "Save your protege, will you? (Coming soon)", path: "#" },
+  { id: "5", name: "Age of Empires 4", genre: "Strategy", posterPath: "images/aoe4.jpg", caption: "Build your civilization, preserve it, conquer others in AOE IV (Coming soon)", path: "#" },
+  { id: "6", name: "Fifa 22", genre: "Sports", posterPath: "images/fifa22.jpeg", caption: "Experience Football with FIFA 22 (Coming soon)", path: "#" },
+  { id: "7", name: "NFSMW", genre: "Racing", posterPath: "images/nfsmw.jpg", caption: "Welcome to Rockport City, the speed has no limit here! (Coming soon)", path: "#" },
+  { id: "8", name: "GTA 5", genre: "Open World Sandbox", posterPath: "images/gta5.jpg", caption: "Master the sandbox world in GTA V (Coming soon)", path: "#" },
 ];
 
 const usersDB = [
